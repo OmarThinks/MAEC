28a04ebb62263f3a7d2820d1694800613709c350
OmarThinks <contact.parmag@gmail.com>
2021-01-20 19:15:17 +0200

    test.py:reciever:added error

--- src/generator.py
+++ src/generator.py
@@ -1,4 +1,14 @@
 def reciever(request, inputs):
+	toReturn = []
+	if type(inputs) != type([]):
+		raise("MOAG:reciever:ERROR:"+
+			" The 'inputs'' varbiale has a type of " + str(type(inputs)) +
+			", type of 'inputs' is supposed to be 'list'.")
+	for inputs_index in inputs:
+		if type(inputs[inputs_index]) != str:
+			raise("MOAG:reciever:ERROR:"+
+			" The 'inputs'' varbiale is supposed to be a list of strings, " + 
+			"one of the elements was found to be "+str(type(inputs[inputs_index])))
 	try:
 		body = request.get_json()
 	except:
