f0b6782cadb5d5ea55d56f1f84e2ee72a45c08f7
OmarThinks <contact.parmag@gmail.com>
2021-01-21 21:10:14 +0200

    init.py:add:supported data types

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 SECRET = "abc"
+DATA_TYPES_SUPPORTED=["string","integer","boolean"]
 try:
 	from .validaton import *
 	from .error import *
