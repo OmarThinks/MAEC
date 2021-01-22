c1e7bbf24008785fffe0c6709d0a08a30b391394
OmarThinks <contact.parmag@gmail.com>
2021-01-22 11:53:00 +0200

    generator.py:renamed:blocks.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,11 +5,11 @@ try:
 	from .validaton import *
 	from .error import *
 	from .models import *
-	from .generator import *
+	from .blocks import *
 	from .test_app import *
 except:
 	from validation import *
 	from error import *
 	from models import *
-	from generator import *
+	from blocks import *
 	from test_app import *
