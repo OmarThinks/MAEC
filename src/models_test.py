eb2104df380a65f761b2139befeec752d30d352e
OmarThinks <contact.parmag@gmail.com>
2021-01-23 03:15:11 +0200

    models_test.py:filled with data

--- src/models_test.py
+++ src/models_test.py
@@ -16,3 +16,82 @@ try:
 	from __init__ import *
 except:
 	from src import *
+
+
+
+
+
+"""
+
+
+b:validation Functions
+
+
+"""
+
+unittest.TestLoader.sortTestMethodsUsing = None
+
+class MoRBs_models_TestCase(unittest.TestCase):
+	"""This class represents the trivia test case"""
+
+	def setUp(self):
+		# create and configure the app
+		self.app = create_app(testing=True) #Flask(__name__)
+		self.client = self.app.test_client
+		#db.app = self.app
+		#db.init_app(self.app)
+		db.create_all()        
+		
+	
+	def tearDown(self):
+		"""Executed after reach test"""
+		print("_+++++++++++++++++++++++++++++++++_")
+
+	#Note: Tests are run alphapetically
+	def test_a_test(self):
+		self.assertEqual(1,1)
+		print("Test 1:Hello, Tests!")
+
+
+
+	def test_002_drop_all_create_all(self):
+		db_drop_and_create_all()
+		products = Product.query.all()
+		self.assertEqual(len(products),0)
+		print("Test 2: db_drop_and_create_all")
+
+
+	def test_003_populate_tables(self):
+		populate_tables()
+		products = Product.query.all()
+		self.assertEqual(len(products),6)
+		print("Test 3: populate tables")
+
+
+
+
+
+	def test_b_01_001_validate_model_id(self):
+		all_products = Product.query
+		validation = validate_model_id(input_id=1,
+			model_query=all_products,
+			model_name_string="product")
+		self.assertEqual(validation["case"],1)
+		self.assertEqual(all_products.get(1),
+			validation["result"])
+		print("Test b_1_1: validate_model_id: Product 1")
+
+
+
+
+
+
+
+
+
+
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+	unittest.main()
