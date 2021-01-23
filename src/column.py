82d467358bb7af2dc50d97a42f1678cbd4c41d7c
OmarThinks <contact.parmag@gmail.com>
2021-01-23 02:52:27 +0200

    column.py:add:imports

---
+++ src/column.py
@@ -0,0 +1,18 @@
+import os
+import secrets
+import unittest
+import json
+import random
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func 
+from flask_cors import CORS
+from flask_migrate import Migrate 
+import base64
+
+import inspect
+
+try:
+	from __init__ import *
+except:
+	from src import *
