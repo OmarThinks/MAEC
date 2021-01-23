a07e5ca393a5f9621d9725cd69d154f8c861d191
OmarThinks <contact.parmag@gmail.com>
2021-01-24 00:07:17 +0200

    objects.py:add:Ckeckpoint

--- src/objects.py
+++ src/objects.py
@@ -281,3 +281,44 @@ def createExpectedFromColumnsDict(inputDict):
 	return toReturn
 
 
+
+
+class Ckeckpoint():
+	"""docstring for Ckeckpoint"""
+	def __init__(self, success, result, input_Class):
+		# Making sure that success is Boolean
+		expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "success",expected_type=bool,
+			input= success)
+		# Making sure that result is dict
+		expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "result",expected_type=dict,
+			input= result)
+		if success == False:
+			# Then we need result to have "status" and "description"
+			expectDictKey(function_name="Checkpoint.__init__",
+				variable_name = "result",expectedKey = "status",
+				input = result)
+			expectDictKey(function_name="Checkpoint.__init__",
+				variable_name = "result",expectedKey = "description",
+				input = result)
+			# Now we need to validate that status is int 
+			# and description is string
+			expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "status",expected_type=int,
+			input= result["status"])
+			expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "description",expected_type=str,
+			input= result["description"])
+			self.success = success
+			self.result = result
+			return
+		# Now succes is boolean and True
+		expected = createExpectedFromClass(inputClass)
+		validate_attendance_from_expected(
+			input_dict= result,input_dict_name = "Checkpont.result",
+			expected= expected)
+		self.success = success
+		self.result = result
+
+
