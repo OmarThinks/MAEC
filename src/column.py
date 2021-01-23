dbfe934529256c086867e1253fe69e9c091a9b62
OmarThinks <contact.parmag@gmail.com>
2021-01-23 03:10:05 +0200

    columns.py:add:DATA_TYPES_SUPPORTED validation

--- src/column.py
+++ src/column.py
@@ -22,13 +22,14 @@ class Column(object):
 	"""docstring for Column"""
 	def __init__(self, name, type):
 		if type(name) != str:
-			data_type_error(function_name="Column",
+			data_type_error(function_name="Column.__init__",
 				variable_name="name",expected_type_name="string",input=name)
 		if type(type) != str:
-			data_type_error(function_name="type",
-				variable_name="name",expected_type_name="string",input=name)
+			data_type_error(function_name="Column.__init__",
+				variable_name="type",expected_type_name="string",input=name)
 		if type not in DATA_TYPES_SUPPORTED:
-			pass
+			not_in_range_error(function_name="Column.__init__",
+				variable_name="type",range=DATA_TYPES_SUPPORTED)
 		self.name = name
 		self.type = type
 		
