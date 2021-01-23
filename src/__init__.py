b3ad8bbc326ae53d70c87296a12c46648d5901ce
OmarThinks <contact.parmag@gmail.com>
2021-01-23 03:23:33 +0200

    init.py:bugfix

--- src/__init__.py
+++ src/__init__.py
@@ -4,16 +4,16 @@ SECRET = "abc"
 DATA_TYPES_SUPPORTED=["string","integer","boolean"]
 
 try:
-	from .column import *
 	from .validaton import *
 	from .error import *
 	from .models import *
+	from .column import *
 	from .level1 import *
 	from .test_app import *
 except:
-	from column import *
 	from validation import *
 	from error import *
 	from models import *
+	from column import *
 	from level1 import *
 	from test_app import *
