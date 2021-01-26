9b0b2c3ee10f6a8a9be979253bbb7fcefc05b759
OmarThinks <contact.parmag@gmail.com>
2021-01-27 00:10:52 +0200

    classreader.py:add:convert_class_to_dict

--- src/objects/classreader.py
+++ src/objects/classreader.py
@@ -1,3 +1,71 @@
+"""
+convert_class_to_dict
+
+- INPUTS: 
+	- input : a class with attributes
+		- Example:
+			class example():
+				name = "Labtop"
+				price = 123
+				expected = "abc" # this will be removed
+	- case : a string that carries one of three values
+		- "all" : means return all attrs
+		- "morbs": get all the attrs containging the word morbs
+		- "clean": get all the attrs NOT containging the word morbs
+- FUNCTION: create a dict that represents the class attributes
+- OUTPUT: a dict that represents the class attrs and their values
+	- Example: {"name":"Labtop","price":123}
+
+"""
+def convert_class_to_dict(input,case = "all"):
+	expectInRange(function_name="convert_class_to_dict",variable_name="case"
+		,range=["all","morbs", "clean"],input = case)
+	data = inspect.getmembers(input, 
+	lambda a:not(inspect.isroutine(a)))
+	to_pop = []
+	
+	the_data = []
+	for a in data:
+		if not(a[0].startswith('_') 
+		or a[0].endswith('_') or
+		(a[0] == "query") or (a[0] == "query_class") or 
+		("expected" in a[0])
+		):
+			the_data.append(a)
+	toReturn = {}
+	for element in the_data:		
+		toReturn[element[0]] = element[1]
+	#Now toReturn is full
+	#To remove all key containng 'morbs'
+	if case == "clean":
+		for key in toReturn:
+			if "morbs" in key:
+				to_pop.append(key)
+	
+	#To remove all keys not containing 'morbs'
+	if case == "morbs":
+		for key in toReturn:
+			if "morbs" not in key:
+				to_pop.append(key)
+
+	#poping useless info
+	for key in to_pop:
+		try:
+			toReturn.pop(key)
+		except Exception as e:
+			pass
+	#Finally
+	return toReturn
+
+
+
+
+
+
+
+
+
+
 
 
 
