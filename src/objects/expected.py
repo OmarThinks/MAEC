25e58eba65a5a522dd9aa0b227ebd26c85613fdf
OmarThinks <contact.parmag@gmail.com>
2021-01-26 23:45:40 +0200

    expected.py:creadted the expected class

---
+++ src/objects/expected.py
@@ -0,0 +1,11 @@
+"""
+expected is an objects that looks consits of one variable
+	- fields
+		this is a list of the expected columns
+		the only way to generate this is to provide the SQLAlchemy model
+"""
+
+
+class expected:
+	def __init__(self, saModel):
+		pass
