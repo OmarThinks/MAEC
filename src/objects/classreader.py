dc41a949d1c907a7cd1330ce8513fe1a3d68704f
OmarThinks <contact.parmag@gmail.com>
2021-01-27 00:08:55 +0200

    classreader.py:mod:renamed sacol

--- src/objects/classreader.py
+++ src/objects/classreader.py
@@ -5,22 +5,23 @@
 
 
 
-def saColumnReader(sacol):
+def saColumnReader(sqlalchmey_column):
 	expectDataType(function_name = "readSAColumn",
-		variable_name = "sacol",expected_type = InstrumentedAttribute,
-		input = sacol)
+		variable_name = "sqlalchmey_column",
+		expected_type = InstrumentedAttribute,
+		input = sqlalchmey_column)
 	toReturn = {}
 	
 	try:
-		toReturn["autoincrement"]=sacol.comparator.autoincrement # auto
-		toReturn["default"]=sacol.comparator.default # None
-		toReturn["foreign_keys"]=sacol.comparator.foreign_keys # set()
-		toReturn["key"]=sacol.comparator.key # id
-		toReturn["name"]=sacol.comparator.name # id
-		toReturn["nullable"]=sacol.comparator.nullable # False
-		toReturn["primary_key"]=sacol.comparator.primary_key # True
-		toReturn["type"]=sacol.comparator.type # INTEGER
-		toReturn["unique"]=sacol.comparator.unique # None
+		toReturn["autoincrement"]=sqlalchmey_column.comparator.autoincrement # auto
+		toReturn["default"]=sqlalchmey_column.comparator.default # None
+		toReturn["foreign_keys"]=sqlalchmey_column.comparator.foreign_keys # set()
+		toReturn["key"]=sqlalchmey_column.comparator.key # id
+		toReturn["name"]=sqlalchmey_column.comparator.name # id
+		toReturn["nullable"]=sqlalchmey_column.comparator.nullable # False
+		toReturn["primary_key"]=sqlalchmey_column.comparator.primary_key # True
+		toReturn["type"]=sqlalchmey_column.comparator.type # INTEGER
+		toReturn["unique"]=sqlalchmey_column.comparator.unique # None
 	except:
 		raise Exception("MoRBs:ERROR:readSAColumn:can not read this"+
 			" column, may be this version of MoRBs is incompatible"+
