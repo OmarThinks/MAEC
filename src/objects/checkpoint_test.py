00e1d69a240c6fb1561423bc487cbea86f98445c
OmarThinks <contact.parmag@gmail.com>
2021-01-28 00:07:57 +0200

    checkpoint_test.py:add:prepare

--- src/objects/checkpoint_test.py
+++ src/objects/checkpoint_test.py
@@ -1,3 +1,10 @@
+import unittest
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import (String, Integer, Float, Boolean)
+from NotReceived import NotReceived
+
+unittest.TestLoader.sortTestMethodsUsing = None
 
 class checkpoint_TestCase(unittest.TestCase):
 
@@ -13,6 +20,19 @@ class checkpoint_TestCase(unittest.TestCase):
 	def test_0_test(self):
 		self.assertEqual(1,1)
 		print("Test 1:Hello, Tests!")
+
+
+
+	def test_1_1_convert_class_to_dict(self):
+		#Testing with a normal class
+		Base = declarative_base()
+		class saTestClass2(Base):
+			__tablename__="hi"
+			id = Column(Integer, primary_key=True, nullable=False)
+			name = Column(String(63))
+			price = Column(Float())
+			in_stock = Column(Boolean())
+		print("Test 1_1:convert_class_to_dict")
 
 
 
