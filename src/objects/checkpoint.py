4d457720fe4a701b349021d5040f1aec88dcabe6
OmarThinks <contact.parmag@gmail.com>
2021-01-27 03:27:22 +0200

    eckpont.py:copied

--- src/objects/checkpoint.py
+++ src/objects/checkpoint.py
@@ -11,6 +11,48 @@ expected is an objects that looks consits of one variable
 """
 
 
-class expected:
-	def __init__(self, saModel):
-		pass
+class Ckeckpoint():
+	"""docstring for Ckeckpoint"""
+	def __init__(self, success, result, inputClass):
+		# Making sure that success is Boolean
+		expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "success",expected_type=bool,
+			input= success)
+		# Making sure that result is dict
+		expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "result",expected_type=dict,
+			input= result)
+		if success == False:
+			# Then we need result to have "status" and "description"
+			expectDictKey(function_name="Checkpoint.__init__",
+				variable_name = "result",expectedKey = "status",
+				input = result)
+			expectDictKey(function_name="Checkpoint.__init__",
+				variable_name = "result",expectedKey = "description",
+				input = result)
+			# Now we need to validate that status is int 
+			# and description is string
+			expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "status",expected_type=int,
+			input= result["status"])
+			expectDataType(function_name= "Checkpoint.__init__",
+			variable_name = "description",expected_type=str,
+			input= result["description"])
+			self.success = success
+			self.result = result
+			return
+		# Now succes is boolean and True
+		expected = createExpectedFromClass(inputClass)
+		validate_attendance_from_expected(
+			input_dict= result,input_dict_name = "Checkpont.result",
+			expected= expected)
+		self.success = success
+		self.result = result
+	def ready():
+		the_dict = self.result
+		for key in the_dict:
+			if the_dict[key] == None:
+				raise Exception(
+				"MoRBs:ERROR:Checkpoint:not ready, but found a value"+
+				" of 'None' in this key:'" + str(key)+"'")
+
