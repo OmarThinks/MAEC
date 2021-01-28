d94caf50f324e8e41719870ff2b9422173104e6d
OmarThinks <contact.parmag@gmail.com>
2021-01-28 05:30:39 +0200

    app.py:add:copied from old example

--- test_app/app.py
+++ test_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -34,6 +34,89 @@ Base.metadata.create_all(bind=engine)
 def home_route():
 	return jsonify({"type(Product)":str(type(Product)),
 		"type(Product.id)":str(type(Product.id))}),200
+
+
+
+
+
+
+
+
+@app.route("/receiver/1")
+def receiver_test_case1():
+	# request is not sent to the method
+
+
+
+
+
+@app.route("/receiver_test/<int:test_case_id>", methods=["POST"])
+def receiver_test(test_case_id):
+	"""
+	This endpont is created to test the receiver endpoint
+	it will return the same exact return value of the receiver function
+	"""
+	"""try:
+		my_request = request
+	except:
+		print("NO",flush=True)"""
+
+	"""try:
+		username = body.get("username",None)
+		password1 = body.get("password1",None)
+		password2 = body.get("password2",None)
+	except:
+		return my_error(status=400, 
+			description = "there is no request body")"""
+	my_request = request
+	expected ={}
+	if test_case_id == 1: #This is successful
+		expected = {"name":"string","price":"string","in_stock":"string"}
+		#This is successful
+	if test_case_id == 2: #This is successful
+		expected = {}
+		#This is successful
+	if test_case_id == 3: #This is the first error
+		my_request = 1
+		#Fail: request should be of type flask.request
+	if test_case_id == 4: #This is the first error
+		my_request = 1
+		expected = {"name":"string","price":"string","in_stock":"string"}
+		#Fail: request should be of type flask.request
+	if test_case_id==5: 
+		#This is second error
+		expected = "This will fail"
+		#Fail: expected should be a list of strings, not a string
+	if test_case_id == 6: #This is the third error (Example:1)
+		expected = ["a","b",["1","2"]]
+		#Fail: There can not be an array inside an array
+	if test_case_id == 7: #This is the third error (Example:2)
+		expected = ["1","c",3]
+		#Fail: Only stirngs are allowed
+	if test_case_id == 8: #This is the third error (Example:2)
+		expected = {"name":"string","price":"string","in_stock":"string"}
+		return receiver(request= my_request,
+			expected=expected,receive_nothing=True)
+		#Fail: Only stirngs are allowed
+	try:
+		result = receiver(request= my_request,expected=expected)
+		if result["success"]==True:
+			return jsonify(result)
+		else:
+			return my_error(fullError=result["result"])
+	except Exception as e:
+		#raise e#print(str(e))
+		return my_error(fullError={"status":500,"description":str(e)})
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
