b0c3e61aab8dc522df48b9c8b7c56082506d63d6
OmarThinks <contact.parmag@gmail.com>
2021-01-28 05:05:48 +0200

    testing database

--- test_app/app.py
+++ test_app/app.py
@@ -11,16 +11,16 @@ app.debug = True
 Base = declarative_base()
 
 class Product(Base):
-    __tablename__ = 'products'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    price = Column(Float)
-    in_stock = Column(Boolean)
-    
+	__tablename__ = 'products'
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	price = Column(Float)
+	in_stock = Column(Boolean)
+	
 @app.route("/")
 def home_route():
-  return jsonify({"message":"Hi There!"}),200
+	return jsonify({"message":"Hi There!"}),200
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
---
+++ test_app/database/README.md
@@ -0,0 +1,1 @@
+testing database will be created here
