44befef0d490e1d553ab2e21fe60e97cc4d4caab
OmarThinks <contact.parmag@gmail.com>
2021-01-28 05:17:04 +0200

    test_app.py:add:connected to db

--- test_app/app.py
+++ test_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import Column, Integer, String, Boolean, Float
 
 
@@ -8,7 +10,12 @@ from sqlalchemy import Column, Integer, String, Boolean, Float
 app = Flask(__name__)
 app.debug = True
 
+engine = create_engine('sqlite:///database/database.sqlite', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
 Base = declarative_base()
+
 
 class Product(Base):
 	__tablename__ = 'products'
@@ -16,6 +23,12 @@ class Product(Base):
 	name = Column(String)
 	price = Column(Float)
 	in_stock = Column(Boolean)
+
+Base.query = db_session.query_property()
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
 	
 @app.route("/")
 def home_route():
